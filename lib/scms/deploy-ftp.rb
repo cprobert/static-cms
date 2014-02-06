f4d80bcabed6b1b915b15140766a80fb3307dbaa
Courtenay Probert <courtenay@probert.me.uk>
2014-02-06 12:14:57 +0000

    Sinc

--- lib/scms/deploy-ftp.rb
+++ lib/scms/deploy-ftp.rb
@@ -1,6 +1,4 @@
-require 'net/ftp'
 require 'fileutils'
-
 require 'scms/extensions.rb'
 require 'scms/scms-utils.rb'
 
@@ -51,13 +49,104 @@ module FtpDeploy
       exit
     end
 
-    ftp = FtpDeploy::Ftp.new(host, port, {:username => username, :password => password, :passive => passive})
-    puts "\r\nConnected to server. Sending site"
-    ftp.sync(website, remote_dir)
-    puts "Successfully sent site"
+    if settings['ssl']
+      ftp = FtpDeploy::Ftps.new(host, port, {:username => username, :password => password, :passive => passive})
+      puts "\r\nConnected to secure server. Sending site"
+      ftp.sync(website, remote_dir)
+      puts "Successfully sent site"
+    else
+      ftp = FtpDeploy::Ftp.new(host, port, {:username => username, :password => password, :passive => passive})
+      puts "\r\nConnected to server. Sending site"
+      ftp.sync(website, remote_dir)
+      puts "Successfully sent site"
+    end
+  end
+
+  class Ftps
+    require 'ftpfxp'
+
+    attr_reader :host, :port, :username, :password, :passive
+
+    def initialize(host, port = 21, options = Hash.new)
+      options = {:username => nil, :password => nil}.merge(options)
+      @host, @port = host, port
+      @username, @password = options[:username], options[:password]
+      @passive = options[:passive]
+    end
+
+    def sync(local, distant)
+      connect do |ftp|
+        send_dir(ftp, local, distant)
+      end
+    end
+
+    private
+    def connect
+      begin
+        ftp = Net::FTPFXPTLS.new
+        ftp.passive = @passive
+        #ftp.debug_mode = true
+        ftp.connect(host, port)
+        ftp.login(username, password)
+
+        puts ftp.welcome
+        yield ftp
+      rescue Exception=>e
+        ScmsUtils.errLog(e.message)
+        ScmsUtils.log(e.backtrace.inspect)
+      end
+    end
+
+    def send_dir(ftp, local, distant)
+      begin
+        ftp.mkdir(distant)
+      rescue Net::FTPPermError
+        # We don't do anything. The directory already exists.
+        # TODO : this is also risen if we don't have write access. Then, we need to raise.
+      end
+      Dir.foreach(local) do |file_name|
+        
+        # If the file/directory is hidden (first character is a dot), we ignore it
+        next if file_name =~ /^(\.|\.\.)$/
+        next if file_name =~ /^(\.)/
+        next if file_name =~ /^_/
+
+        #puts file_name
+
+        if ::File.stat(local + "/" + file_name).directory?
+          # It is a directory, we recursively send it
+          begin
+            ftp.mkdir(distant + "/" + file_name)
+          rescue Net::FTPPermError
+            # We don't do anything. The directory already exists.
+            # TODO : this is also risen if we don't have write access. Then, we need to raise.
+          end
+          send_dir(ftp, local + "/" + file_name, distant + "/" + file_name)
+        else
+           # It's a file, we just send it
+           localFilePath = local + "/" + file_name
+           remoteFilePath = distant + "/" + file_name
+
+           if File.binary?(local + "/" + file_name)
+             puts "#{file_name} (binary)"
+             ftp.putbinaryfile(localFilePath, remoteFilePath)
+           else
+             puts "#{file_name} (text)"
+             ftp.puttextfile(localFilePath, remoteFilePath)
+           end
+        end
+      end
+    end
+
+    private
+    def host_with_port
+      "#{host}:#{port}"
+    end
   end
 
   class Ftp
+    require 'net/ftp'
+
     attr_reader :host, :port, :username, :password, :passive
 
     def initialize(host, port = 21, options = Hash.new)
